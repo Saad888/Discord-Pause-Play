30b1bc6b5fa5cd04ebaae52292aca107a334e165
Xaad <Saad888@users.noreply.github.com>
2019-06-09 20:36:49 -0600

    donezo

--- Discord Party Play/main.py
+++ Discord Party Play/main.py
@@ -11,13 +11,12 @@ class PauseClient(discord.Client):
         self.keyboard = Controller()
         self.pause_key = KeyCode.from_vk(0xB3)
         self.space_key = KeyCode.from_vk(0x20)
-        self.selector = True  # True for space command, False for pause
+        self.selector = False  # True for space command, False for pause
         self.pause_strings = ['!pause', '!play']
         self.start_bot()
 
     def start_bot(self):
         """Starts bot client"""
-        token = self.token
         loop = asyncio.get_event_loop()
         loop.create_task(self.start(self.token))
         try:
@@ -33,15 +32,21 @@ class PauseClient(discord.Client):
 
     async def on_message(self, message):
         """Defines user inputs"""
+        chan_id = message.channel.id
+        accepted_channels = [586681429069398202, 586336266702946334]
+        if chan_id not in accepted_channels:
+            return
+
         if message.author.id == self.user.id:
             return
         
         def matcher(msg, match):
-            return msg.startswith(f'!{match}')
+            return msg.startswith(f'!{match.lower()}')
 
         async def send_key():
             key_name = 'space' if self.selector is True else 'media pause/play'
             reply = f'Currently using {key_name}'
+            print(reply)
             await message.channel.send(reply)
 
         msg = message.content.lower()
@@ -64,15 +69,22 @@ class PauseClient(discord.Client):
             reply = 'This is a test'
             await message.channel.send(reply)
 
-        if matcher(msg, 'help'):
-            reply = '!pause or !play - play/pause the media\n'
-            reply += '!swtich - change key from space to pause/play VK\n'
+        if matcher(msg, 'PMP_help'):
+            reply = 'This bot will send either a pause/play key hit (like a '
+            reply += 'shortcut on a keyboard) or the space bar whenever someone ' 
+            reply += 'types !pause or !play in this discord channel. ' 
+            reply += 'Keep this running in command prompt or powershell in the '
+            reply += 'background. \n'
+            reply += 'Commands: \n'
+            reply += '!pause or !play - play/pause the media\n'
+            reply += '!switch - swaps between sending pause/play or sending' 
+            reply += 'space when the discord command is used\n'
             reply += '!key_space - change key output to space\n'
             reply += '!key_pause_play - change key output to space\n'
-            reply += '!pauser_exit - shut down the bot'
+            reply += '!PMP_exit - shut down the bot'
             await message.channel.send(reply)
 
-        if matcher(msg, 'pauser_exit'):
+        if matcher(msg, 'PMP_exit'):
             await message.channel.send('Leaving')
             await self.logout()
             loop = asyncio.get_running_loop()
@@ -80,9 +92,8 @@ class PauseClient(discord.Client):
 
     async def on_ready(self):
         print('loaded')
-        game = discord.Game('use !help')
+        game = discord.Game('use !PMP_help')
         await self.change_presence(status=discord.Status.idle, activity=game)
-
 
 token = ''
 with open('token.txt', 'r') as file:
